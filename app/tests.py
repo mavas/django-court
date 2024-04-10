302427f28df2b81beeb7c5864241fefecf395ea7
mavas <10604882+mavas@users.noreply.github.com>
2024-04-10 14:10:41 -0700

    Add initial test

--- app/tests.py
+++ app/tests.py
@@ -5,3 +5,8 @@ class Basic(TestCase):
     def test_basic(self):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 404)
+
+
+class Allauth(TestCase):
+    def test_basic(self):
+        import allauth
